ff1653ed0a87c0e388689662f29f222f98e2e793
Aakash <aakashsittu28@gmail.com>
2023-06-25 04:00:34 +0530

    api changed

--- src/pages/FetchData.jsx
+++ src/pages/FetchData.jsx
@@ -6,7 +6,8 @@ const FetchData = () => {
 
     const [Data, setData] = useState('')
     const fetchdata = async () => {
-        await axios.get("https://newsapi.org/v2/everything?q=russia&pageSize=10&apiKey=ad55dddb4f4d43c580a924f837ef61d9").then((res) => setData(res.data.articles))
+        await axios.get("https://newsapi.in/newsapi/search.php?key=x4sYPg30zwEaZXbW2QzskpYXiIU3rm&q=russia").then((res) => setData(res.data.News))
+
     }
     useEffect(() => { fetchdata() }, [])
 
@@ -16,8 +17,8 @@ const FetchData = () => {
                 {Data ? Data.map((item, ind) => <>
                     <div className='container my-3' style={{ width: "750px", boxShadow: "2px 2px 10px silver", borderRadius: "10px" }}>
                         <h5 className='my-3 '>{item.title}</h5>
-                        <div className='d-flex justify-content-center align-items-center'><img src={item.urlToImage} alt="Link broken..." className='image-fluid' style={{ width: "auto", height: "270px", objectFit: "cover" }} /></div>
-                        <p>{item.content.substring(0, 200)}</p>
+
+                        <p>{item.description.substring(0, 200)}</p>
                         <a href={item.url} target="blank">Read the Full Article Here</a>
                     </div>
                 </>) : "Fetching...."}
