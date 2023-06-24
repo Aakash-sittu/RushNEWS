b0b631764bbfc81bda2745faea9397a182e3e514
Aakash <aakashsittu28@gmail.com>
2023-06-25 03:44:04 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
         <div>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">RUS-UKR</a>
+                    <a class="navbar-brand" href="https://teal-bubblegum-21a12a.netlify.app">RUS-UKR</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <a class="nav-link " aria-current="page" href="#">Home</a>
+                            <a class="nav-link " aria-current="page" href="https://teal-bubblegum-21a12a.netlify.app">Home</a>
                             <a class="nav-link " href="https://t.me/mod_russia/">Russian MOD</a>
                             <a class="nav-link" href="https://wagner-pmc.com">PMC Wagner</a>
                         </div>
